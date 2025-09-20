82daa83b9a9f90e33c7a35022c72b0d3329afd87
braiki89 <aymen.braiki@esprit.tn>
2025-09-20 21:55:22 +0200

    test jenkinsfile

--- src/main/java/pidev/eventconnect/services/StripeService.java
+++ src/main/java/pidev/eventconnect/services/StripeService.java
@@ -53,7 +53,7 @@ public class StripeService {
         } catch (StripeException e) {
             
         }
-
+       //jenkins file
         return StripeResponse
                 .builder()
                 .status("SUCCESS")
